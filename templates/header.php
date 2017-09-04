1a8d1a5044ef39b4cfb4b7864d270d0fcf77dac9
Clément Ducerf <clementducerf@gmail.com>
2017-09-04 22:21:04 +0200

    Adding logo, welcome image, fonts

--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,5 @@
 <header class="banner">
+  <img id="logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/LFA-logo.svg" alt="Les films d'Argile"/>
   <div class="container">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
     <nav class="nav-primary">
