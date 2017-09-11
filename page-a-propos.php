b963c38ad61f7e548c7c1b88c664daffa8540792
Clément Ducerf <9747465+clementducerf@users.noreply.github.com>
2017-09-12 00:09:17 +0200

    A propos & Contact page are now dynamic -content stored in database with advanced custom field

--- page-a-propos.php
+++ page-a-propos.php
@@ -4,57 +4,24 @@
  ?>
     <?php get_template_part('templates/page', 'header'); ?>
     <div class="introduction">
-        <p>
-        Créée en 2014, la société <b>Les Films d’Argile</b> regroupe trois personnes : <b>Alice Bégon</b>, <b>Clément Schneider</b> (respectivement diplômés de <b>La fémis</b> dans les départements production et réalisation) et <b>Chloé Chevalier</b> (diplômée en Master de Cinéma à Paris III).
-        </p>
+        <?php if(get_field('apropos_introduction')){
+            echo get_field('apropos_introduction');
+        } ?>
     </div>
     <div class="deuxcolonnes">
-        <p>
-            <b>Nous avons fondé notre structure sur deux bases solides&nbsp;:</b><br/>
-            •&nbsp;Une longue activité de production sous la forme d’une association étudiante (fondée, elle, en 2008) qui fut le laboratoire brouillon des premières expériences et via laquelle nous avons produit, en 2012, le long-métrage <em>Études pour un paysage amoureux.</em> Distribué en salle (Saint- André-des-Arts et province), le film a rencontré un public enthousiaste, qui nous a encouragés à persévérer dans cette voie de l’autoproduction et de l’indépendance.
-        </p>
-        <div class="columnbreak"></div>
-        <p>
-            •&nbsp;La complémentarité de nos trois profils : Une productrice forte d’une riche expérience (au sein de la société <b>Stromboli Films</b>, elle produit notamment le court métrage <em>Jeunesse des loups Garous</em> de <b>Yann Delattre</b> sélectionné à la <em>Semaine de la critique 2015</em>). Un producteur-réalisateur-projectionniste en cours de rédaction d’une thèse sur les rapports entre cinéma et utopie. Une scénariste et romancière auteure de la saga de fantasy <em>Récits du Demi-Loup</em> publiée aux éditions <b>Les Moutons Électriques.</b> Ce qui nous rassemble est le désir profond de raconter des histoires, nos histoires en préservant l’intransigeance de nos envies et, naturellement, notre liberté de créateurs.
-        </p>
+        <?php if(get_field('apropos_structure')){
+            echo get_field('apropos_structure');
+        } ?>
     </div>
     <div class="conclusion">
-        <p>
-            Aux Films d’Argile, savoirs et savoir-faire se mêlent joyeusement sans cloisonnisme ni domaines réservés. C’est comme cela que nous gardons les yeux ouverts, c’est comme cela que tout résonne et que prime, au bout du compte, le plaisir de fabriquer ensemble, collectivement, des objets singuliers et sans équivalents dont nous
-            pouvons, sans rougir, dire profondément qu’ils sont nôtres.
-        </p>
+        <?php if(get_field('apropos_developpement')){
+            echo get_field('apropos_developpement');
+        } ?>
     </div>
     <div class="troiscolonnes">
-        <div class="img-container" style="
-        background: url(<?php echo get_template_directory_uri(); ?>/assets/images/apropos/alice-begon.jpg) no-repeat center;
-        -webkit-background-size: cover; /* pour anciens Chrome et Safari */
-        background-size: cover; /* version standardisée */
-        background-position: center top;
-        ">
-        </div>
-        <h2>Alice Bégon</h2>
-        <div class="columnbreak"></div>
-        <div class="img-container" style="
-            background: url(<?php echo get_template_directory_uri(); ?>/assets/images/apropos/chloe-chevalier.jpg) no-repeat center;
-            -webkit-background-size: cover; /* pour anciens Chrome et Safari */
-            background-size: cover; /* version standardisée */
-            background-position: center top;
-            ">
-        </div>
-        <h2>Chloé Chevalier</h2>
-        <p>Née en 1988, titulaire d’un Master de cinéma sur la fantasy, <b>Chloé Chevalier</b> commence par naviguer entre plusieurs activités – assistanat, production – avant de se focaliser essentiellement sur l’écriture. Trois longs métrages de sa plume sont actuellement en développement chez <b>les Films d’Argile.</b> Son expérience de l’écriture ne se limite pas au scénario puisque <b>Chloé Chevalier</b> écrit aussi des romans – sa série <em>Récits du Demi-Loup</em>, en quatre tomes, est publiée chez <b>les Moutons Électriques</b> – confirmant par la même occasion son goût pour les intrigues amples et la création d’univers imaginaires complexes.</p>
-        <div class="columnbreak"></div>
-        <div class="img-container" style="
-            background: url(<?php echo get_template_directory_uri(); ?>/assets/images/apropos/clement-schneider.jpg) no-repeat center;
-            -webkit-background-size: cover; /* pour anciens Chrome et Safari */
-            background-size: cover; /* version standardisée */
-            background-position: center top;
-            ">
-        </div>
-        <h2>Clément Schneider</h2>
-        <p>Né en 1989, <b>Clément Schneider</b> suit d’abord une classe préparatoire de cinéma. Un passage par <b>Louis-Lumière</b>, avant d’entrer à la <b>Fémis</b>, dans le département Réalisation. Il profite des rencontres et des possibilités que lui offre l’école pour réaliser le long-métrage Études pour un paysage amoureux. À la sortie de l’école, il fonde avec <b>Chloé Chevalier</b> et <b>Alice Bégon</b> la société <b>Les Films d’Argile</b>, afin de prolonger son travail de cinéaste indépendant, tout en travaillant comme projectionniste. Aujourd’hui, il démarre en outre une thèse sur un sujet qui le travaille depuis longtemps&#8239;: <em>les relations entre le cinéma et l’utopie.</em></p>
+        <?php if(get_field('apropos_membres')){
+            echo get_field('apropos_membres');
+        } ?>
     </div>
     <?php get_template_part('templates/content', 'page'); ?>
-
-
 </div>
--- page-contact.php
+++ page-contact.php
@@ -4,27 +4,14 @@
     while (have_posts()) : the_post(); ?>
 <?php get_template_part('templates/page', 'header'); ?>
 <div class="deuxcolonnes">
-    <p>
-        <b>&#99;&#111;&#110;&#116;&#97;&#99;&#116;@&#108;&#101;&#115;&#102;&#105;&#108;&#109;&#115;&#100;&#97;&#114;&#103;&#105;&#108;&#101;.&#102;&#114;</b><br/><br/>
-        <a href="tel:0951980413">09 51 98 04 13</a><br/><br/>
-        adresse des bureaux<br/>
-        <b>Les films d'Argile<br/>
-        45 rue de Tolbiac<br/>
-        75013 Paris</b><br/><br/>
-        adresse du siège social<br/>
-        <b>Les films d'Argile<br/>
-        Maison des Vendanges,<br/>
-        1 rue du Merle<br/>
-        71250 Cluny</b>
-        <br/><br/>
-        <a href="https://www.facebook.com/Les-Films-dArgile-373675522719895/" target="_blank">facebook</a>
-    </p><br/>
+    <?php if(get_field('contact_colonne1')){
+        echo get_field('contact_colonne1');
+    } ?>
 </div>
 <div class="deuxcolonnes">
-    <p>
-        colophon<br/><br/>
-        Le site à été decidé à deux mains par l’atelier <a href="http://pierrefeuilleciseau.fr" target="_blank">Pierre Feuille Ciseau</a>, en <em>Jannon Sans</em> et développé par <a href="http://www.clementducerf.com" target="_blank">Clément Ducerf</a> en 2017.
-    </p>
+    <?php if(get_field('contact_colonne2')){
+        echo get_field('contact_colonne2');
+    } ?>
 </div>
 <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
